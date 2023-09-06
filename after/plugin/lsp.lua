888e9b9c231d80280e7c74e18913d8f146938ee8
Andreas Wågø Wilson <AndreasWago.Wilson@skatteetaten.no>
2023-09-06 20:29:59 +0200

    remove revive

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -75,7 +75,6 @@ null_ls.setup({
     null_ls.builtins.formatting.gofumpt,
     null_ls.builtins.formatting.goimports,
     null_ls.builtins.diagnostics.golangci_lint,
-    null_ls.builtins.diagnostics.revive,
     null_ls.builtins.formatting.black
   }
 })
