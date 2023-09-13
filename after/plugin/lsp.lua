6f58bc189a7f807cdf284727a1eb795ae5123c2d
Andreas Wågø Wilson <AndreasWago.Wilson@skatteetaten.no>
2023-09-13 12:57:56 +0200

    add keybindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,7 +21,27 @@ lsp_zero.on_attach(function(client, bufnr)
   -- to learn the available actions
   lsp_zero.default_keymaps({ buffer = bufnr })
   lsp_zero.buffer_autoformat()
-  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
+  -- Add keymaps. Mainly as default, but adding them for reference
+  vim.keymap.set('n', 'rn', '<cmd>lua vim.lsp.buf.rename()<cr>', { buffer = true, desc = "LSP rename" })
+  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', { buffer = true, desc = "Show LSP hover information" })
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', { buffer = true, desc = "Go to LSP definition" })
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', { buffer = true, desc = "Go to LSP declaration" })
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>',
+    { buffer = true, desc = "Go to LSP implementation" })
+  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>',
+    { buffer = true, desc = "Go to LSP type definition" })
+  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>',
+    { buffer = true, desc = "Show LSP signature help" })
+  vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>',
+    { buffer = true, desc = "Trigger LSP code actions" })
+  vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>',
+    { buffer = true, desc = "Open diagnostics floating window" })
+  vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>',
+    { buffer = true, desc = "Go to previous diagnostic location" })
+  vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>',
+    { buffer = true, desc = "Go to next diagnostic location" })
+  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>',
+    { buffer = true, desc = "Search LSP references using Telescope" })
 end)
 
 
